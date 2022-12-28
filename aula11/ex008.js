c86302ce0e7f34e284b15a4e682c77bbc0f8b063
José Júnior <59178745+josec-junior@users.noreply.github.com>
2022-12-28 04:12:09 -0300

    Repositório Esvaziado

--- aula11/ex008.js
+++
@@ -1,5 +0,0 @@
-var vel = 60
-console.log(`A velocidade do seu carro é ${vel} Km/h`)
-if(vel > 60) { // Condição Simples
-    console.log("Você ultrapassou a velocidade permitida. MULTADO!")
-}
--- aula16/funcao03.js
+++
@@ -1,5 +0,0 @@
-var v = function(x) { /* Variável recebendo uma função */
-    return x*2
-}
-
-console.log(v(5))
